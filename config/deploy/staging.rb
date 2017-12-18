edaeeef0d382495dd95862d12319c688ffa93029
Pine Wong <pinewong@163.com>
2017-12-18 17:01:26 +0800

    Update

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,5 @@
+set :branch, "staging"
+server "aws.urlis.cn", user: "deploy", roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
